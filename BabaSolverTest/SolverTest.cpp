401015300ba9cae3a8f474221effce1083ed9adf
Mark Langer <s.mark.langer@gmail.com>
2025-05-23 01:18:40 -0400

    Fix SolverTest.cpp
    Just need to add the level name parameter to the `Solve()` call.

--- BabaSolverTest/SolverTest.cpp
+++ BabaSolverTest/SolverTest.cpp
@@ -9,7 +9,7 @@ TEST(SolverTest, FindsSolution)
 {
 	std::shared_ptr<BabaSolver::GameState> initial_state = BabaSolver::TestLevel();
 	BabaSolver::SolverOptions options;
-	std::shared_ptr<BabaSolver::GameState> end_state = BabaSolver::Solve(initial_state, options);
+	std::shared_ptr<BabaSolver::GameState> end_state = BabaSolver::Solve("Test Level", initial_state, options);
 	ASSERT_TRUE(end_state);
 	EXPECT_TRUE(end_state->HaveWon());
 }
